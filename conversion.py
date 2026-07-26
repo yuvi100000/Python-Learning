74ce15c976f5f87738e215e5526ca81f4ddb981e
Banti Singh <bantising82953@gmail.com>
2026-07-26 17:12:39 +0530

    Added input function example

---
+++ conversion.py
@@ -0,0 +1,11 @@
+# type conversion
+a = 1
+b = 2.0
+c = a + b
+print(c) # output is 3.0 because of type conversion 
+print(type(c)) # output is <class 'float'> because of type conversion 
+n = float("19") 
+o = 2
+m = int(1.2
+)
+print(type(o+m)) 
---
+++ input.py
@@ -0,0 +1,21 @@
+
+# Python Input Function Example
+# Author: Banti Singh
+# Description:
+# This program demonstrates how to take user
+# input and check the data type of each input.
+
+# Taking user's name
+name = input("Enter your name: ")
+print("Name:", name)
+print("Data Type:", type(name))
+
+# Taking user's roll number
+rollno = input("Enter your roll number: ")
+print("Roll Number:", rollno)
+print("Data Type:", type(rollno))
+
+# Taking user's email
+contact_email = input("Enter your Email ID: ")
+print("Email:", contact_email)
+print("Data Type:", type(contact_email))
